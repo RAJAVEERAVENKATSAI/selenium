b4a6e8c6f472268dfca58e385e1106fa84ec708c
venka <venka@VenkatSai>
2025-04-16 11:23:55 +0530

    Success1

--- src/locators/DragDrop.java
+++ src/locators/DragDrop.java
@@ -18,7 +18,8 @@ public class DragDrop {
 		WebDriverManager.chromedriver().setup();
       WebDriver driver = new ChromeDriver();
       driver.manage().window().maximize();
-   
+   // JUNIOR NTR
+      
       Actions sai = new Actions(driver); 
       driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
       driver.get("https://jqueryui.com/droppable/");
